632dcc79deb9c7aa9a9fad18d6cf65c76733c9d6
Matthew Pagan <mastapegs01@gmail.com>
2021-11-18 06:10:06 -0500

    define grid column lines

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,43 @@
+use dominator::{class, html, Dom};
 use once_cell::sync::Lazy;
-use dominator::{Dom, html, class};
 use wasm_bindgen::prelude::*;
 
 struct App {}
 
 impl App {
     fn render() -> Dom {
-        static MAIN_GRID_CONTAINER: Lazy<String> = Lazy::new(|| class! {
-          .style("display", "grid")
+        static MAIN_GRID_CONTAINER: Lazy<String> = Lazy::new(|| {
+            class! {
+              .style("display", "grid")
+              .style("grid-template-columns", "[jig-pics] 200px [jig-name] auto [author] auto [author-badge] auto [date] auto [instr-lang] auto [curators] auto")
+            }
         });
-      
+
         html!("div", {
           .class(&*MAIN_GRID_CONTAINER)
-          .text("Hello, World!")
+          .children(&mut [
+            html!("div", {
+              .text("Jig Pictures")
+            }),
+            html!("div", {
+              .text("Jig Name")
+            }),
+            html!("div", {
+              .text("Author")
+            }),
+            html!("div", {
+              .text("Author's Badge")
+            }),
+            html!("div", {
+              .text("Date")
+            }),
+            html!("div", {
+              .text("Instruction Language")
+            }),
+            html!("div", {
+              .text("Curators")
+            }),
+          ])
         })
     }
 }
