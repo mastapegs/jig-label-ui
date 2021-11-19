dd2352f81c9a6a681ab8cdb5e6aedc8f713fdb8f
Matthew Pagan <mastapegs01@gmail.com>
2021-11-18 19:49:06 -0500

    headers are not mutable

--- src/lib.rs
+++ src/lib.rs
@@ -5,68 +5,68 @@ use std::sync::Arc;
 use wasm_bindgen::prelude::*;
 
 struct App<'a> {
-    headers: Mutable<Vec<&'a str>>,
+  headers: Mutable<Vec<&'a str>>,
 }
 
 impl App<'_> {
-    fn new() -> Arc<Self> {
-        Arc::new(Self {
-            headers: Mutable::new(vec![
-                "Jig Pictures",
-                "Jig Name",
-                "Author",
-                "Author's Badge",
-                "Date",
-                "Instruction Language",
-                "Curators",
-            ]),
-        })
-    }
+  fn new() -> Arc<Self> {
+    Arc::new(Self {
+      headers: vec![
+        "Jig Pictures",
+        "Jig Name",
+        "Author",
+        "Author's Badge",
+        "Date",
+        "Instruction Language",
+        "Curators",
+      ],
+    })
+  }
 
-    fn render(state: Arc<Self>) -> Dom {
-        static MAIN_GRID_CONTAINER: Lazy<String> = Lazy::new(|| {
-            class! {
-              .style("display", "grid")
-              .style("grid-template-columns", "[jig-pics] 200px [jig-name] auto [author] auto [author-badge] auto [date] auto [instr-lang] auto [curators] auto")
-            }
-        });
+  fn render(state: Arc<Self>) -> Dom {
+    static MAIN_GRID_CONTAINER: Lazy<String> = Lazy::new(|| {
+      class! {
+        .style("display", "grid")
+        .style("grid-template-columns", "[jig-pics] 200px [jig-name] auto [author] auto [author-badge] auto [date] auto [instr-lang] auto [curators] auto")
+      }
+    });
 
+    html!("div", {
+      .class(&*MAIN_GRID_CONTAINER)
+      .children(&mut [
         html!("div", {
-          .class(&*MAIN_GRID_CONTAINER)
-          .children(&mut [
-            html!("div", {
-              .text("Jig Pictures")
-            }),
-            html!("div", {
-              .text("Jig Name")
-            }),
-            html!("div", {
-              .text("Author")
-            }),
-            html!("div", {
-              .text("Author's Badge")
-            }),
-            html!("div", {
-              .text("Date")
-            }),
-            html!("div", {
-              .text("Instruction Language")
-            }),
-            html!("div", {
-              .text("Curators")
-            }),
-          ])
-        })
-    }
+          .text("Jig Pictures")
+        }),
+        html!("div", {
+          .text("Jig Name")
+        }),
+        html!("div", {
+          .text("Author")
+        }),
+        html!("div", {
+          .text("Author's Badge")
+        }),
+        html!("div", {
+          .text("Date")
+        }),
+        html!("div", {
+          .text("Instruction Language")
+        }),
+        html!("div", {
+          .text("Curators")
+        }),
+      ])
+    })
+  }
 }
 
 #[wasm_bindgen(start)]
 pub fn main_js() -> Result<(), JsValue> {
-    #[cfg(debug_assertions)]
-    console_error_panic_hook::set_once();
+  #[cfg(debug_assertions)]
+  console_error_panic_hook::set_once();
 
-    let app = App::new();
-    dominator::append_dom(&dominator::body(), App::render(app));
+  let app = App::new();
+  dominator::append_dom(&dominator::body(), App::render(app));
 
-    Ok(())
+  Ok(())
 }
