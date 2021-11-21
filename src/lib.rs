001d9ecb10382ec10fd56a54b4fa62b9beaef393
Matthew Pagan <mastapegs01@gmail.com>
2021-11-21 16:58:51 -0500

    working on rendering jigs

--- src/lib.rs
+++ src/lib.rs
@@ -12,26 +12,28 @@ struct JigData {
   curators: Vec<String>,
 }
 
-struct App<'a> {
-  headers: Vec<&'a str>,
+struct App {
+  headers: Vec<String>,
+  jigs: Vec<JigData>,
 }
 
-impl App<'_> {
-  fn new() -> Arc<Self> {
+impl App {
+  fn new(headers: Vec<String>, jigs: Vec<JigData>) -> Arc<Self> {
     Arc::new(Self {
-      headers: vec![
-        "Jig Name",
-        "Author",
-        "Author's Badge",
-        "Date",
-        "Instruction Language",
-        "Curators",
-      ],
+      headers: headers,
+      jigs: jigs,
     })
   }
 
   fn render(app: Arc<Self>) -> Dom {
     static HEADER_FLEX_CONTAINER: Lazy<String> = Lazy::new(|| {
+      class! {
+        .style("display", "flex")
+        .style("justify-content", "space-between")
+      }
+    });
+
+    static JIGS_CONTAINER: Lazy<String> = Lazy::new(|| {
       class! {
         .style("display", "flex")
         .style("justify-content", "space-between")
@@ -49,13 +51,32 @@ impl App<'_> {
           }))
         }),
         html!("div", {
-          .class(&*HEADER_FLEX_CONTAINER)
-          .children(app.headers.iter().map(|header| {
+          .children(app.jigs.iter().map(|jig| {
             html!("div", {
-              .text(header)
+              .class(&*JIGS_CONTAINER)
+              .children([
+                html!("div", {
+                  .text(&jig.jig_name)
+                }),
+                html!("div", {
+                  .text(&jig.author)
+                }),
+                html!("div", {
+                  .text(&jig.author_badge)
+                }),
+                html!("div", {
+                  .text(&jig.date)
+                }),
+                html!("div", {
+                  .text(&jig.language)
+                }),
+                html!("div", {
+                  .text(&jig.curators[0])
+                }),
+              ])
             })
           }))
-        })
+        }),
       ])
     })
   }
@@ -66,7 +87,23 @@ pub fn main_js() -> Result<(), JsValue> {
   #[cfg(debug_assertions)]
   console_error_panic_hook::set_once();
 
-  let app = App::new();
+  let jigs: Vec<JigData> = vec![JigData {
+    jig_name: String::from("name"),
+    author: String::from("author"),
+    author_badge: String::from("badge"),
+    date: String::from("date"),
+    language: String::from("language"),
+    curators: vec![String::from("person1")],
+  }];
+  let jig_headers = vec![
+    String::from("Jig Name"),
+    String::from("Author"),
+    String::from("Author's Badge"),
+    String::from("Date"),
+    String::from("Instruction Language"),
+    String::from("Curators"),
+  ];
+  let app = App::new(jig_headers, jigs);
   dominator::append_dom(&dominator::body(), App::render(app));
 
   Ok(())
