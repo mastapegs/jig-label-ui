7ebdb5637a0ef71b96ec2c5d031250f06121fd9f
Matthew Pagan <mastapegs01@gmail.com>
2021-11-23 12:22:11 -0500

    added typescript component for JigLabelUI

---
+++ stories/jig-label-ui/JigLabelUI.stories.ts
@@ -0,0 +1,8 @@
+import { html } from "lit";
+import "./JigLabelUI";
+
+export default {
+  title: "Jig Label UI",
+};
+
+export const Primary = () => html`<jib-label-ui></jib-label-ui>`;
---
+++ stories/jig-label-ui/JigLabelUI.ts
@@ -0,0 +1,9 @@
+import { LitElement, html } from "lit";
+import { customElement } from "lit/decorators";
+
+@customElement("jib-label-ui")
+export class JigLabelUI extends LitElement {
+  render() {
+    return html`<p>Jig Label UI</p>`;
+  }
+}
