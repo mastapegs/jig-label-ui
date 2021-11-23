186bb873c4353008adc80b9b73f0b5beb3ac1a2b
Matthew Pagan <mastapegs01@gmail.com>
2021-11-23 15:46:55 -0500

    bundling lit

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,7 +19,7 @@ module.exports = {
   },
   rules: {
     "import/extensions": [
-      "error",
+      "off",
       "ignorePackages",
       {
         js: "never",
--- rollup.config.js
+++ rollup.config.js
@@ -2,31 +2,47 @@ import rust from "@wasm-tool/rollup-plugin-rust";
 import serve from "rollup-plugin-serve";
 import livereload from "rollup-plugin-livereload";
 import { terser } from "rollup-plugin-terser";
+import typescript from "@rollup/plugin-typescript";
+import resolve from "@rollup/plugin-node-resolve";
+import common from "@rollup/plugin-commonjs";
 
 const isWatch = !!process.env.ROLLUP_WATCH;
 
-export default {
-  input: {
-    index: "./Cargo.toml",
-  },
-  output: {
-    dir: "dist/js",
-    format: "iife",
-    sourcemap: true,
-  },
-  plugins: [
-    rust({
-      serverPath: "js/",
-    }),
-
-    isWatch &&
-      serve({
-        contentBase: "dist",
-        open: true,
+export default [
+  {
+    input: {
+      dominator: "./Cargo.toml",
+    },
+    output: {
+      name: "dominator",
+      dir: "dist/js",
+      format: "iife",
+    },
+    plugins: [
+      rust({
+        serverPath: "js/",
       }),
 
-    isWatch && livereload("dist"),
+      isWatch &&
+        serve({
+          contentBase: "dist",
+          open: true,
+        }),
 
-    !isWatch && terser(),
-  ],
-};
+      isWatch && livereload("dist"),
+
+      !isWatch && terser(),
+    ],
+  },
+  {
+    input: {
+      components: "stories/jig-label-ui/jig-label-ui.ts",
+    },
+    output: {
+      name: "components",
+      dir: "dist/js",
+      format: "iife",
+    },
+    plugins: [resolve(), common(), typescript()],
+  },
+];
